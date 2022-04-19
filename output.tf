112ce5f4c4c5a8c44b130cf7f58318332c36d630
Datsent <45486622+Datsent@users.noreply.github.com>
2022-04-19 11:22:00 +0300

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,4 +1,4 @@
 output "instance_ip_addr" {
   value       = "Open in browser: ${aws_alb.load_balancer.dns_name}"
-  description = "The private IP address of the main server instance."
+  description = "Load Balancer DNS name"
 }
